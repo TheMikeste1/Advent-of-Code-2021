6127c4612ea6c65ee9df8d5a67df3d91efae6519
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2021-12-02 09:37:36 -0700

    Day01 Part 2 Complete

--- Day01/day01.py
+++ Day01/day01.py
@@ -1,5 +1,7 @@
+# %% Part 1
 puzzle_in = [int(val) for val in open("input", "r").readlines()]
+print(f"Part 1: {sum([1 if pair[0] < pair[1] else 0 for pair in zip(puzzle_in[:-1], puzzle_in[1:])])}")
 
-count = sum([1 if pair[0] < pair[1] else 0 for pair in zip(puzzle_in[:-1], puzzle_in[1:])])
-
-print(count)
+# %% Part 2
+sliding_window_sums = list(map(sum, zip(puzzle_in[:-2], puzzle_in[1:-1], puzzle_in[2:])))
+print(f"Part 2: {sum([1 if pair[0] < pair[1] else 0 for pair in zip(sliding_window_sums[:-1], sliding_window_sums[1:])])}")
